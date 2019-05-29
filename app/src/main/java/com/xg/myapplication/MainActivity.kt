7a5014cf66ced5e9ec192238e36ea074ff8dcf5a
xiegang <1272108979@qq.com>
2019-05-29 09:54:58 +0800

    Revert "测试回滚type1"
    This reverts commit f4a0ac93ce559ca0925c72bb548bc54bfd1d2b5a.

--- app/src/main/java/com/xg/myapplication/MainActivity.kt
+++ app/src/main/java/com/xg/myapplication/MainActivity.kt
@@ -15,8 +15,6 @@ class MainActivity : AppCompatActivity() {
         //testThree()
         // testFour()
         // testFive()
-
-        //测试git操作333333333333333333333333333333333333
         testSix()
     }
 
